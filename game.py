039e3633e54cebdefdc190a0262583bd3ae039f2
lapikale <lapidario.kaela@gmail.com>
2025-12-06 16:34:16 -0600

    adding bg image to screen

--- game.py
+++ game.py
@@ -8,12 +8,18 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Make Your Character!')
 running = True
 
+#add bg
+bg_image = pygame.image.load('bg.png ')
+bg_image_rect = bg_image.get_rect()
+bg_image_rect.topleft(0,0)
+
 while running:
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill('pink')
+    #adding bg to screen
+    display_surface.blit(bg_image, bg_image_rect)
 
     pygame.display.flip() 
 
