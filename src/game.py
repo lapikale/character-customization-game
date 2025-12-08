a99c98d5f00ef09187b3316ec5d2d6bedd4ff58d
lapikale <lapidario.kaela@gmail.com>
2025-12-07 20:31:01 -0600

    reimplemented previous code

--- src/game.py
+++ src/game.py
@@ -22,30 +22,12 @@ bg_image_rect.topleft = (0,0)
 
 class Head:
     def __init__(self):
-        #load images in a list
-        head01 = pygame.image.load('PFDA Final Images/head01.png')
-        head01_rect = head01.get_rect()
-        head01_rect.topleft = (0,0)
-        head02 = pygame.image.load('PFDA Final Images/head02.png')
-        head02_rect = head02.get_rect()
-        head02_rect.topleft = (0,0)
-        head03 = pygame.image.load('PFDA Final Images/head03.png')
-        head03_rect = head03.get_rect()
-        head03_rect.topleft = (0,0)
-        self.images = [head01, head02, head03]
-
-        def iterate_through_list(self, images):
-            index = 0
-            self.images = images[index]
-            change_image_event = pygame.USEREVENT +1
-
-            running = True
-            while running:
-                for event in pygame.event.get():
-                    if event.type == change_image_event:
-                        index += 1
-                        index %= len(self.images)
-                        image = images[index]
+        self.pos = (0,0)
+        image_paths = ['PFDA Final Images/head01.png', 'PFDA Final Images/head02.png', 'PFDA Final Images/head03.png']
+        self.images = [pygame.image.load(path).convert_alpha() for path in image_paths]
+        self.current_image_index = 0
+        self.image = self.images[self.current_image_index]
+        self.rect = self.image.get_rect(topleft = (self.pos))
         
         #for image in range(len(self.images)):
             #self.images.append(pygame.image.load(self.image_files).convert_alpha)
@@ -63,11 +45,17 @@ class Head:
         #toggle image visibility when selected
         #for image in self.images:
             #surf.draw(self.images, self.pos)
-        surface.blit(self.images, (0,0))
+        surface.blit(self.image, self.rect)
+
+    def set_image_by_index(self, index):
+        if 0 <= index < len(self.images):
+            self.current_image_index = index
+            self.image = self.images[self.current_image_index]
+            self. rect = self.image.get_rect(topleft = (self.pos))
     
 
 while running:
-    #instancing object
+    #instancing objects
     head = Head()
 
     for event in pygame.event.get(): 
@@ -75,7 +63,13 @@ while running:
             running = False
         
         #changing image with key press
-        
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                next_index = (head.current_image_index + 1) % len(head.images)
+                head.set_image_by_index(next_index)
+            elif event.key == pygame.K_LEFT:
+                prev_index = (head.current_image_index - 1 + len(head.images)) % len(head.images)
+                head.set_image_by_index(prev_index)
             
     #image visibility toggle
         #if event.type == pygame.KEYDOWN:
