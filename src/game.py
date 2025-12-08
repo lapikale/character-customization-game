1c9bdb97c946d334c0931150d85c8b0958c314c4
lapikale <lapidario.kaela@gmail.com>
2025-12-07 23:45:59 -0600

    button class implemented and arrow images updated

--- src/game.py
+++ src/game.py
@@ -21,14 +21,28 @@ class Button:
     def __init__(self, x, y, image):
         self.image = image
         self.rect = self.image.get_rect()
-        self.rect.topleft = (x,y)
+        self.rect.topleft = (x, y)
+        self.clicked = False
 
     def draw(self):
+        action = False
+        #mouse logic
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.clicked = True
+                action = True
+
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+        
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
+        return action
+
 #button instances
-right_button = Button(50, 200, right_arrow)
-left_button = Button(-50, 200, left_arrow)
+right_button = Button(50, -50, right_arrow)
+left_button = Button(-50, -50, left_arrow)
 
 
 class Head:
@@ -186,8 +200,10 @@ while running:
     top.draw(screen)
 
     #drawing buttons to screen
-    right_button.draw()
-    left_button.draw()
+    if right_button.draw():
+        print('RIGHT')
+    if left_button.draw():
+        print('LEFT')
 
     pygame.display.flip() 
     pygame.display.update()
